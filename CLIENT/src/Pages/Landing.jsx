4fef812b2fc45e5165b54ed0d6dd48f1c8356982
Vuk <vukvukvukt@gmail.com>
2026-01-12 23:37:33 +0100

    Rad na services sekciji

--- CLIENT/src/Pages/Landing.jsx
+++ CLIENT/src/Pages/Landing.jsx
@@ -1,11 +1,13 @@
 import NavBar from "../Components/NavBar/Navbar.jsx";
 import Hero from "../Components/Hero/Hero.jsx";
+import Services from "../Components/Services/Services.jsx"
 
 function LandingPage(){
     return (
         <div>
             <NavBar/>
             <Hero/>
+            <Services/>
         </div>
     )
 }
