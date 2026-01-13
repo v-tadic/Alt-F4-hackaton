0a717334d27b049af51cdbae896eb64ecfacea87
Vuk <vukvukvukt@gmail.com>
2026-01-13 15:57:20 +0100

    Odradjen login page

--- CLIENT/src/Pages/Login.jsx
+++ CLIENT/src/Pages/Login.jsx
@@ -1,9 +1,44 @@
+import {useState} from "react";
+import { Link } from "react-router-dom";
+import styles from "../Css/Login.module.css";
+
 function LoginPage(){
+    const [isLogIn, setIsLogIn] = useState(true);
+
+    function handleLoginChange(){
+        setIsLogIn(!isLogIn);
+    }
+
     return (
-        <div>
-            <p>Login page</p>
+        <div className={styles.Container}>
+            {isLogIn ? (
+                <div className={styles.FormContainer}>
+                    <form>
+                        <input name="EmailLogIn" type="text" placeholder="Enter Your Email:" className={styles.InputField} />
+                        <input name="PasswordLogIn" type="password" placeholder="Enter Your Password:" className={styles.InputField} />
+                        <button type="Submit" className={styles.Button}>Login</button>
+                    </form>
+                    <div className={styles.ButtomButtonDiv}>
+                        <button onClick={handleLoginChange} className={styles.ToggleLogin}>{isLogIn ? "Sign Up Instead" : "Log In Instead"}</button>
+                        <Link to={"/"}>Back To Home</Link>
+                    </div>
+                </div>
+            ) : (
+                <div className={styles.FormContainer}>
+                    <form>
+                        <input name="EmailSignUp" type="text" placeholder="Enter Your Email:" className={styles.InputField} />
+                        <input name="PasswordSignUp" type="password" placeholder="Enter Your Password:" className={styles.InputField} />
+                        <input name="Username" type="text" placeholder="Enter Your username:" className={styles.InputField} />
+                        <button type="Submit" className={styles.Button}>Sign Up</button>
+                    </form>
+                    <div className={styles.ButtomButtonDiv}>
+                        <button onClick={handleLoginChange} className={styles.ToggleLogin}>{isLogIn ? "Sign Up Instead" : "Log In Instead"}</button>
+                        <Link to={"/"}>Back To Home</Link>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
 export default LoginPage;
